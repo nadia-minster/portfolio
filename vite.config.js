60a2396803f4bb821e7419ace1fb9a16388c7d00
nadia <petrovanadia96@gmail.com>
2023-06-04 14:39:34 +1000

    f

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginFaviconsInject("src/assets/favicon-32x32.png")],
+  plugins: [react()],
 });

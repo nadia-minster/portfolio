908802a76c1b74b282400bc7355d09c444886fc0
nadia <petrovanadia96@gmail.com>
2023-06-04 17:58:43 +1000

    hero style

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import nadia from "../assets/Nadia.png";
 import sphere from "../assets/sphere.png";
+import texture from "../assets/glitch-effect-black-by-rawpixel.jpg";
 
 const Hero = () => {
   return (
     <section className="main hero">
+      <img className="hero-texture" src={texture} alt="glitch texture" />
       <img src={sphere} alt="decorative sphere" className="hero-sphere" />
       <img src={nadia} alt="Nadia Minster" className="hero-nadia" />
-      <h1>Web developer</h1>
+      <h1 className="heading-hero">Web developer</h1>
     </section>
   );
 };

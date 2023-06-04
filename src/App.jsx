f26cb2fbb9ffbb0b3c629d10d034fb900f903475
nadia <petrovanadia96@gmail.com>
2023-06-04 14:30:48 +1000

    favicon 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import Hero from "./Sections/Hero";
 import Nav from "./Sections/Nav";
 import About from "./Sections/About";
 import Projects from "./Sections/Projects/Projects";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import Footer from "./Sections/Footer";
 import "./index.css";
 
 function App() {
   return (
     <main>
-      <Nav />
-      <Hero />
-      <About />
-      <Projects />
-      <Footer />
+      <HelmetProvider>
+        <Helmet>
+          å<title>Nadia Minster — Portfolio</title>
+          <link rel="icon" type="icon/ico" href="dist/assets/icon.ico" />
+        </Helmet>
+        <Nav />
+        <Hero />
+        <About />
+        <Projects />
+        <Footer />
+      </HelmetProvider>
     </main>
   );
 }

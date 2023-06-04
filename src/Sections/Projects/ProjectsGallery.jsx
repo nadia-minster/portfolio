90cf4dac4484d24892236f629da2cf3f5fca947d
nadia <petrovanadia96@gmail.com>
2023-06-05 08:56:35 +1000

    hero

--- src/Sections/Projects/ProjectsGallery.jsx
+++ src/Sections/Projects/ProjectsGallery.jsx
@@ -1,8 +1,18 @@
+import movie from "../../assets/project-images/movie.png";
+import recipe from "../../assets/project-images/recipe.png";
+import travel from "../../assets/project-images/travel.png";
+
 const ProjectsGallery = ({ project }) => {
   console.log(project);
   return (
     <div className="project-gallery">
-      <img src={project.image} alt={project.name} className="gallery-image" />
+      {project.image === "movie" ? (
+        <img src={movie} alt={project.name} className="gallery-image" />
+      ) : project.image === "recipe" ? (
+        <img src={recipe} alt={project.name} className="gallery-image" />
+      ) : (
+        <img src={travel} alt={project.name} className="gallery-image" />
+      )}
       <div className="gallery-description">
         <h3 className="name">{project.name}</h3>
         <h3 className="type">{project.type}</h3>

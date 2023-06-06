d6d5764dbd5d01e2a84f1779c5b34a93d679c606
nadia <petrovanadia96@gmail.com>
2023-06-06 13:08:58 +1000

    resume fix

--- src/Sections/Nav.jsx
+++ src/Sections/Nav.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { FaArrowDown } from "react-icons/fa";
 
 const Nav = () => {
+  const handleDownload = () => {
+    window.open("/Nadia Minster – Resume.pdf");
+  };
   return (
     <nav>
       <ul>
         <li>About</li>
         <li>Projects</li>
       </ul>
-      <button>
+      <button onClick={() => handleDownload()}>
         <FaArrowDown className="resume-icon" />
         Resume
       </button>

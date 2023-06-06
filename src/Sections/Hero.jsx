d19c60736fdc67231ed4133cc27ca69f8ade8f34
nadia <petrovanadia96@gmail.com>
2023-06-06 21:01:12 +1000

    attempt at fixing overflow issue

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -9,7 +9,9 @@ const Hero = () => {
   return (
     <section className="main hero">
       <img className="hero-texture" src={texture} alt="glitch texture" />
-      <img src={sphere} alt="decorative sphere" className="hero-sphere" />
+      <div className="hide-overflow">
+        <img src={sphere} alt="decorative sphere" className="hero-sphere" />
+      </div>
       <img src={nadia} alt="Nadia Minster" className="hero-nadia" />
       <h1 className="heading-hero">Web developer</h1>
       <Link

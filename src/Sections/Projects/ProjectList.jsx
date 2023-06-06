21f42a463f898d2cc4ee26486f0dcff0d5cfc210
nadia <petrovanadia96@gmail.com>
2023-06-06 16:40:44 +1000

    added project description

--- src/Sections/Projects/ProjectList.jsx
+++ src/Sections/Projects/ProjectList.jsx
@@ -1,5 +1,18 @@
 import { FaSortDown, FaSortUp } from "react-icons/fa";
 import { useState } from "react";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaFigma,
+  FaGithub,
+  FaEnvelope,
+} from "react-icons/fa";
+import movie from "../../assets/project-images/movie-list-image.png";
+import recipe from "../../assets/project-images/recipe.png";
+import travel from "../../assets/project-images/travel.png";
 
 const ProjectList = ({ project }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,9 +31,26 @@ const ProjectList = ({ project }) => {
           </button>
         </div>
 
-        <div
-          className={`project-list-desc ${!isOpen ? "closed" : "open"}`}
-        ></div>
+        <div className={`project-list-desc ${!isOpen ? "closed" : "open"}`}>
+          {project.description.map((paragraph, index) => (
+            <p className="project-description" key={index}>
+              {paragraph}
+            </p>
+          ))}
+          <div className="list-tech-icons">
+            <FaHtml5 />
+            <FaCss3Alt />
+            <FaJs />
+            <FaReact />
+            <FaFigma />
+          </div>
+          <img
+            src={project.id === 1 ? movie : project.id === 2 ? recipe : travel}
+            alt={project.name}
+            className="list-image"
+            onClick={() => window.open(project.link)}
+          />
+        </div>
       </div>
     </div>
   );
